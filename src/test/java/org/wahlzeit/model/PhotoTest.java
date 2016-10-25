240009bf4ec9d0b0f6f42a2f9b31a08a52970837
Marcel <marcel.swoboda@gmx.de>
2016-10-25 14:45:15 +0200

    add fruther photo tests

--- src/test/java/org/wahlzeit/model/PhotoTest.java
+++ src/test/java/org/wahlzeit/model/PhotoTest.java
@@ -3,6 +3,7 @@ package org.wahlzeit.model;
 import com.googlecode.objectify.ObjectifyService;
 import com.googlecode.objectify.Work;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
@@ -16,14 +17,27 @@ import static org.junit.Assert.assertNotNull;
  */
 public class PhotoTest {
 
+	private Photo photo;
+
+	@Before
+	public void init() {
+		photo = new Photo();
+	}
 
 	@Test
 	public void testPhotoLocation() {
-		Photo photo = new Photo();
 		Location location = new Location();
 		photo.setLocation(location);
 
 		Assert.assertSame(location, photo.getLocation());
+	}
 
+	@Test
+	public void testDefaultOfLocationIsNull() {
+		Assert.assertNull(photo.getLocation());
+	}
+	@Test
+	public void testLocationCanSetToNullWithoutException(){
+		photo.setLocation(null);
 	}
 }
