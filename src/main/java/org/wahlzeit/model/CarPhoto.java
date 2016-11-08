26e7644fc5b27703b51600dc4afb13dcef464519
Marcel <marcel.swoboda@gmx.de>
2016-11-08 15:05:32 +0100

    create CarPHoto, CarPhotoFactory and CarPhotoManager with tests

---
+++ src/main/java/org/wahlzeit/model/CarPhoto.java
@@ -0,0 +1,7 @@
+package org.wahlzeit.model;
+
+/**
+ * Created by Marcel on 08.11.2016.
+ */
+public class CarPhoto {
+}
---
+++ src/test/java/org/wahlzeit/model/CarPhotoManagerTest.java
@@ -0,0 +1,7 @@
+package org.wahlzeit.servlets;
+
+/**
+ * Created by Marcel on 08.11.2016.
+ */
+public class CarPhotoManager {
+}
